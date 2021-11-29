9b5c7cdc427c9d17a9a1f8bded2c128eca1cfdf0
martamagui <marta.molina@live.u-tad.com>
2021-11-29 13:37:06 +0100

    Reformat Code

--- app/src/main/java/com/example/demom04netwroktechnologies/TechnologyListFragment.kt
+++ app/src/main/java/com/example/demom04netwroktechnologies/TechnologyListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.demom04netwroktechnologies.databinding.FragmentTechnologyListBinding
 
 
@@ -13,6 +14,7 @@ class TechnologyListFragment : Fragment() {
     private var _binding: FragmentTechnologyListBinding? = null
     private val binding
         get() = _binding!!
+    private val adapter = TechnologyAdpter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,10 +26,15 @@ class TechnologyListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        configUI()
+    }
+    private fun configUI(){
         binding.fabAddTech.setOnClickListener {
             val action = TechnologyListFragmentDirections.actionTechnologyListFragmentToTechnologyAddFragment()
             findNavController().navigate(action)
         }
+        binding.rvTechnology.layoutManager = GridLayoutManager(context,2)
+        binding.rvTechnology.adapter = adapter
     }
     override fun onDestroy() {
         super.onDestroy()
