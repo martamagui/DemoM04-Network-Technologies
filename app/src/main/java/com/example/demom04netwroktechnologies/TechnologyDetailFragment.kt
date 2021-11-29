09d806796e195062348550a3b446ae9bc323a88a
martamagui <marta.molina@live.u-tad.com>
2021-11-29 14:45:55 +0100

    DetailView

--- app/src/main/java/com/example/demom04netwroktechnologies/TechnologyDetailFragment.kt
+++ app/src/main/java/com/example/demom04netwroktechnologies/TechnologyDetailFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.navArgs
 import com.example.demom04netwroktechnologies.databinding.FragmentTechnologyDetailBinding
 import com.example.demom04netwroktechnologies.databinding.FragmentTechnologyListBinding
 
@@ -12,6 +14,7 @@ class TechnologyDetailFragment : Fragment() {
     private var _binding: FragmentTechnologyDetailBinding? = null
     private val binding
         get() = _binding!!
+    private  val args : TechnologyDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +24,11 @@ class TechnologyDetailFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(context, "TechId: ${args.techId}", Toast.LENGTH_SHORT).show()
+        binding.tvId.text = args.techId
+    }
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
